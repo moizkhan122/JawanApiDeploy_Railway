edb0f2d72494925b6cda8ce2863ac5d6a00c7479
Moiz khan <68587163+moizkhan122@users.noreply.github.com>
2023-04-22 21:13:32 +0500

    f

--- EcommerceApiJawan/settings.py
+++ EcommerceApiJawan/settings.py
@@ -54,9 +54,12 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
+     'DEFAULT_RENDERER_CLASSES': ( 
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 ROOT_URLCONF = 'EcommerceApiJawan.urls'
 
